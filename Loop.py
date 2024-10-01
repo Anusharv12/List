8f4fea0a90d1d4aebd302718bb735a8f76c16d70
Anusharv <anusharathna512@gmail.com>
2024-09-30 22:50:06 -0700

    Update and rename README.md to Loop.py

---
+++ Loop.py
@@ -0,0 +1,24 @@
+# List
+'''1)Write a program to create a list and display it. 
+Input format:
+Input consist of n+1 integers
+First integer corresponds to the size of the list
+Next n inputs corresponds to the elements in the list 
+Output format: 
+Output is an integer list
+Sample Input
+4 
+1
+2
+3
+4
+Output
+[1, 2, 3, 4]'''
+Ans:
+size = int(input())
+elements = []
+for _ in range(size):
+    element = int(input())
+    elements.append(element)
+print(elements)
+
